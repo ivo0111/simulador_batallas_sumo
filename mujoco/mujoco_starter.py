3723036d5d9fca4a73f9bb23e24b246964e69a57
ivo0111 <ivogiovarruscio@gmail.com>
2026-03-23 23:31:12 -0300

    Robot y separación de xml's

--- mujoco/mujoco_starter.py
+++ mujoco/mujoco_starter.py
@@ -1,13 +1,32 @@
 import mujoco
 import mujoco.viewer
 
-with open("arena.xml") as f:
+with open("scene.xml") as f:
     xml = f.read()
 
 model = mujoco.MjModel.from_xml_string(xml)
 data = mujoco.MjData(model)
 
+mujoco.mj_saveLastXML("scene_compiled.xml", model)
+
+for i in range(model.njnt):
+    name = mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_JOINT, i)
+    dof_adr = model.jnt_dofadr[i]
+    print(f"Joint {i}: '{name}' → DOF {dof_adr}")
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
+    t = 0
     while viewer.is_running():
+        t += model.opt.timestep
+
+        if t < 2.0:
+            # Avanza recto
+            data.ctrl[0] = 8.0
+            data.ctrl[1] = 8.0
+        else:
+            # Gira
+            data.ctrl[0] = 8.0
+            data.ctrl[1] = -8.0
+
         mujoco.mj_step(model, data)
         viewer.sync()
